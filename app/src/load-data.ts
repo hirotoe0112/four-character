c205a683cea178189401159c8ac7ac1668fdfe3f
hirotoe0112 <hirotoe0112@gmail.com>
2023-12-08 16:51:40 +0900

    yamlでデータ登録できる機能を追加

---
+++ app/src/load-data.ts
@@ -0,0 +1,20 @@
+import { AppDataSource } from './data-source'
+import { parse } from 'yaml'
+import fs from 'fs'
+import path from 'path'
+import { FourCharacter } from './entity/four-character'
+
+type Character = {
+  entity: string,
+  items: FourCharacter[]
+}
+
+const loadData = async () => {
+  await AppDataSource.initialize()
+
+  const file = fs.readFileSync(path.resolve(__dirname, './data/four-characters.yml'), 'utf8')
+  const parsedData: Character = parse(file)
+
+  await FourCharacter.save(parsedData.items)
+}
+void loadData()
